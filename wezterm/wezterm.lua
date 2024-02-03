f74dfaf0e1b070b3d10bad27258c7afdb915ef8b
Sergei Zyubin <git@szyubin.me>
2024-02-03 15:51:49 +0100

    feat(wezterm): add some commands to the palette, cleanup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,11 @@
 local wezterm = require 'wezterm';
-local config = wezterm.config_builder()
+local config = wezterm.config_builder();
 
 config.font = wezterm.font_with_fallback({ "JetBrains Mono", "Apple Color Emoji" })
 config.font_size = 14.0
+
 config.initial_cols = 120
 config.initial_rows = 30
---config.window_background_opacity = 0.8
---config.color_scheme = "Mirage"
 config.color_scheme = "Catppuccin Mocha"
 config.hide_tab_bar_if_only_one_tab = true
 config.scrollback_lines = 10000
@@ -14,11 +13,14 @@ config.tab_bar_at_bottom = true
 config.use_cap_height_to_scale_fallback_fonts = true
 config.adjust_window_size_when_changing_font_size = false
 config.custom_block_glyphs = true
+
+
+-- Shortcuts
 config.keys = {
     { key = 'PageUp',   mods = 'SUPER', action = wezterm.action.ScrollByPage(-0.5) },
     { key = 'PageDown', mods = 'SUPER', action = wezterm.action.ScrollByPage(0.5) },
     {
-    -- This will create a new split and run your default program inside it
+        -- This will create a new split and run your default program inside it
         key = "Enter",
         mods = "CTRL|SHIFT",
         action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } }
@@ -44,18 +46,73 @@ config.keys = {
             end),
         },
     },
-    {
-        key = 'R',
-        mods = 'CMD|SHIFT',
-        action = wezterm.action.PromptInputLine {
-            description = 'Enter new name for tab',
-            action = wezterm.action_callback(function(window, pane, line)
-                if line then
-                    window:active_tab():set_title(line)
-                end
-            end)
-        }
-    }
 }
+
+-- # Events
+
+wezterm.on('augment-command-palette', function(window, pane)
+    return {
+        {
+            -- Toggle background opacity
+            brief = 'Window: Toggle background opacity',
+
+            action = wezterm.action_callback(function(window, pane)
+                local overrides = window:get_config_overrides() or {}
+                if not overrides.window_background_opacity then
+                    overrides.window_background_opacity = 0.8
+                else
+                    overrides.window_background_opacity = nil
+                end
+                window:set_config_overrides(overrides)
+            end),
+        },
+        {
+            -- Add 'Rename Tab' action to the command palette
+            brief = 'Window: Rename tab',
+            icon = 'md_rename_box',
+
+            action = wezterm.action.PromptInputLine {
+                description = 'Enter new name for tab',
+                action = wezterm.action_callback(function(window, pane, line)
+                    if line then
+                        window:active_tab():set_title(line)
+                    end
+                end),
+            },
+        },
+        {
+            -- Toggle 'Cozette Vector' bitmap-like font yet can be scaled
+            brief = 'Toggle Bitmap Font: Cozette Vector',
+            icon = 'md_format_font',
+            action = wezterm.action_callback(function(window, pane)
+                local overrides = window:get_config_overrides() or {}
+                if not overrides.font then
+                    overrides.font = wezterm.font_with_fallback({ "CozetteVector", "Apple Color Emoji" })
+                    overrides.font_size = 18.1
+                else
+                    overrides.font = nil
+                    overrides.font_size = nil
+                end
+                window:set_config_overrides(overrides)
+            end),
+        },
+        {
+            -- Toggle 'Cozette HiDpi' bitmap font. Sharp but has a single size
+            brief = 'Toggle Bitmap Font: Cozette HiDpi',
+            icon = 'md_format_font',
+            action = wezterm.action_callback(function(window, pane)
+                local overrides = window:get_config_overrides() or {}
+                if not overrides.font then
+                    overrides.font = wezterm.font_with_fallback({ "CozetteHiDpi", "Apple Color Emoji" })
+                else
+                    overrides.font = nil
+                    overrides.font_size = nil
+                end
+                window:set_config_overrides(overrides)
+            end),
+        }
+
+    }
+end)
 
 return config
