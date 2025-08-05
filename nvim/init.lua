71c1f6ccaa4652fd6065209422a2cd7b4094e763
Sergei Zyubin <git@szyubin.me>
2025-08-05 19:48:00 +0200

    feat(nvim): add oil

--- nvim/init.lua
+++ nvim/init.lua
@@ -711,6 +711,32 @@ local lazy_plugins = {
         }
     },
     {
+        "stevearc/oil.nvim",
+        cmd = "Oil",
+        config = function()
+            require("oil").setup({
+                default_file_explorer = false,
+                delete_to_trash = true,
+                skip_confirm_for_simple_edits = false,
+                columns = { 'icon', 'permissions', 'size' },
+                view_options = {
+                    show_hidden = true,
+                    is_always_hidden = function(name, _)
+                        return name == '..' or name == '.git'
+                    end,
+                },
+                win_options = {
+                    signcolumn = "yes",
+                    foldcolumn = "0",
+                    conceallevel = 3,
+                    concealcursor = "nvic",
+                },
+                watch_for_changes = true,
+                use_default_keymaps = true,
+            })
+        end
+    },
+    {
         "AckslD/nvim-neoclip.lua",
         event = "TextYankPost",
         config = function()
